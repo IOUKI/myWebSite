933a43ea68d2a99ee93f7ffb5f93f64057c7d6d6
IOUKI <quw112233456@gmail.com>
2023-04-16 21:06:54 +0800

    new navbar

--- src/components/blockLayout.vue
+++ src/components/blockLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="w-full h-screen">
+    <section class="w-full h-screen bg-gray-900">
         <slot></slot>
     </section>
 </template>
--- src/views/blocks/Title.vue
+++ src/views/blocks/Title.vue
@@ -1,7 +1,7 @@
 <template>
     <blockLayout>
-        <div class="w-full h-full bg-gray-900 flex justify-center items-center">
-            <h1 class="text-3xl text-sky-500">
+        <div class="w-full h-full flex justify-center items-center">
+            <h1 class="text-3xl text-sky-500 animate-ping">
                 Hello lan
             </h1>
         </div>
